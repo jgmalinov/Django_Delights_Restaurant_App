bea83e5d8df7456f9ca0b3c34ad56a0628cc1f89
jgmalinov <gogotkd24@gmail.com>
2022-12-28 15:45:49 +0200

    Initial model version created and migrations applied

--- Django_Delights_App/restaurant/models.py
+++ Django_Delights_App/restaurant/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Ingredients(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    price = models.FloatField()
+    quantity_available = models.FloatField()
+
+    class Meta:
+        ordering = ["name"]
+
+class MenuItems(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+
+    class Meta:
+        ordering = ["name"]
+
+class RecipeRequirements(models.Model):
+    ingredient_id = models.ForeignKey(Ingredients, on_delete=models.PROTECT)
+    menu_item_id = models.ForeignKey(MenuItems, on_delete=models.CASCADE)
+    quantity_needed = models.FloatField()
+
+    def is_sufficient(self, quantity_available):
+        return self.quantity_needed >= quantity_available
+
+
+class Purchases(models.Model):
+    menu_item_id = models.ForeignKey(MenuItems, on_delete=models.PROTECT)
+    time_of_purchase = models.DateTimeField()
+    bill = models.FloatField()
+
+    class Meta:
+        ordering = ["time_of_purchase"]
