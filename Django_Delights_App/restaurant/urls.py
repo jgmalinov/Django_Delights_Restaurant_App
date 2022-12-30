27de88fad7d46d70a0d58363e105802e1024445d
jgmalinov <gogotkd24@gmail.com>
2022-12-30 20:31:50 +0200

    Functioning registration and login views, certain bugs in terms of redirecting after authentication and a

--- Django_Delights_App/restaurant/urls.py
+++ Django_Delights_App/restaurant/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_view),
-    path('login/', include('django.contrib.auth.urls')),
+    path('', views.home_view, name="home"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup', views.signup_view, name="signup"),
     path('logout/', views.logout_view, name="logout"),
     path('about/', views.about_view, name="about")
 ]
--- Django_Delights_App/restaurant/views.py
+++ Django_Delights_App/restaurant/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+def signup_view(request):
+    print(request.POST)
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+        return render(request, "registration/signup.html", {"form": form})
 @login_required
 def home_view(request):
     context = {"username": request.user.username}
