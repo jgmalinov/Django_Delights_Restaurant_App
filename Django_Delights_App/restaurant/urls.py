02161714dfbe569d52ad6c25bd4bbbba1694dca3
jgmalinov <gogotkd24@gmail.com>
2023-01-02 14:04:17 +0200

    Improvements to the auth system, implemented 3 new ListViews, for reviewing the inventory, purchase log and restaurant menu

--- Django_Delights_App/restaurant/urls.py
+++ Django_Delights_App/restaurant/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     path('accounts/profile/', views.home_view, name="home"),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup', views.signup_view, name="signup"),
+    path('accounts/signup/', views.signup_view, name="signup"),
     path('logout/', views.logout_view, name="logout"),
+    path('accounts/inventory/', views.InventoryList.as_view(), name = "inventory"),
+    path('accounts/menu/', views.MenuItemList.as_view(), name = "menu"),
+    path('accounts/purchase/', views.PurchaseList.as_view(), name = "purchase"),
     path('about/', views.about_view, name="about")
 ]
--- Django_Delights_App/restaurant/views.py
+++ Django_Delights_App/restaurant/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.views.generic import ListView
+from .models import Ingredient, MenuItem, Purchase
 # Create your views here.
 
 def signup_view(request):
@@ -19,14 +22,32 @@ def signup_view(request):
     else:
         form = UserCreationForm()
         return render(request, "registration/signup.html", {"form": form})
-@login_required
-def home_view(request):
-    context = {"username": request.user.username}
-    return render(request, "restaurant/home.html", context)
 
 def logout_view(request):
     logout(request)
     return redirect("home")
 
+
+@login_required
+def home_view(request):
+    context = {"username": request.user.username}
+    return render(request, "restaurant/home.html", context)
+
+@login_required
 def about_view(request):
     return render("restaurant/about.html")
+
+class InventoryList(LoginRequiredMixin, ListView):
+    model = Ingredient
+
+
+class MenuItemList(LoginRequiredMixin, ListView):
+    model = MenuItem
+
+class PurchaseList(LoginRequiredMixin, ListView):
+    model = Purchase
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["MenuItem"] = 'laina'
+        return context
