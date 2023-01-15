53931b1bbae08d5ea2b336243e3e5c88f83123cc
jgmalinov <99488455+jgmalinov@users.noreply.github.com>
2023-01-15 16:15:41 +0200

    Delete Django_Delights_App directory

--- Django_Delights_App/restaurant/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
-# Register your models here.
-admin.site.register(Ingredient)
-admin.site.register(MenuItem)
-admin.site.register(RecipeRequirement)
-admin.site.register(Purchase)
